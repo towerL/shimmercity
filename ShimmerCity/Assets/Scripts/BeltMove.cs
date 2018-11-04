5a29f2ef74b168e4f72db0237a09bea7769923f0
towerL <513298163@qq.com>
2018-11-04 21:45:01 +0800

    test

---
+++ ShimmerCity/Assets/Scripts/BeltMove.cs
@@ -0,0 +1,19 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class BeltMove : MonoBehaviour {
+//    //目标位置
+//    public Transform target;
+//    //移动距离
+//    public float dis;
+
+//    void Start () {
+//        dis = Vector2.Distance(transform.position, target.position);
+//    }
+
+//    void Update()
+//    {
+//        transform.position = Vector2.MoveTowards(transform.position, target.position, (dis / 3f) * Time.deltaTime);
+//    }
+//}
