84a10641dea98484ea517d5030126c9c31454a19
towerL <513298163@qq.com>
2018-11-05 22:11:20 +0800

    1.为传送带添加移动效果; 2.删除梯子的碰撞体; 3.修改中景动画; 4.添加锤子漂浮效果

--- ShimmerCity/Assets/Scripts/BeltMove.cs
+++ ShimmerCity/Assets/Scripts/BeltMove.cs
@@ -1,19 +1,41 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class BeltMove : MonoBehaviour {
-//    //目标位置
-//    public Transform target;
-//    //移动距离
-//    public float dis;
+public class BeltMove : MonoBehaviour
+{
+    //移动距离
+    public float dis;
+    //移动速度
+    public float speed;
+    //水平移动为true 竖直移动为false
+    public bool flag;
+    //移动方向
+    float dir = 1;
+    //起始位置
+    float start_x;
+    float start_y;
 
-//    void Start () {
-//        dis = Vector2.Distance(transform.position, target.position);
-//    }
+    void Start()
+    {
+        start_x = transform.position.x;//水平移动的起点
+        start_y = transform.position.y;//竖直移动的起点
+    }
 
-//    void Update()
-//    {
-//        transform.position = Vector2.MoveTowards(transform.position, target.position, (dis / 3f) * Time.deltaTime);
-//    }
-//}
+    void Update()
+    {
+        //gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, target.position, (dis / 3f) * Time.deltaTime);
+        //移动
+        //竖直移动
+        if (flag)
+        {
+            transform.Translate(Vector2.right * dir * speed * Time.deltaTime);
+            if (System.Math.Abs(transform.position.x - start_x) <= 0.1f || System.Math.Abs(transform.position.x - start_x) >= dis) { dir = -dir; }
+        }
+        //水平移动
+        else {
+            transform.Translate(Vector2.up * dir * speed * Time.deltaTime);
+            if (System.Math.Abs(transform.position.y - start_y) <= 0.1f || System.Math.Abs(transform.position.y - start_y) >= dis) { dir = -dir; }
+        }
+    }
+}
