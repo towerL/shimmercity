16a147ee0309a9310e32a75cb335d382fd2af147
La745739773 <745739773@qq.com>
2018-11-14 09:49:00 +0800

    modify deerbug scripts

--- ShimmerCity/Assets/Scripts/EnemyController.cs
+++ ShimmerCity/Assets/Scripts/EnemyController.cs
@@ -7,16 +7,41 @@ public class EnemyController : MonoBehaviour {
     //方向
     private int direction = 1;
 
+    public Transform target;
+
+    public GameObject curTraget,lastTraget;
+
+    public float _distance;
+
+    private Animator animator;
+
+    private bool bisAttacking;
     // Use this for initialization
     void Start () {
         direction = -1;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //移动
-        transform.Translate(Vector2.right * direction * 2f * Time.deltaTime);
+        if (bisAttacking == false)
+        {
+            transform.Translate(Vector2.right * direction * 2f * Time.deltaTime);
+        }
+        float distance = (transform.position - target.position).sqrMagnitude;
+        Debug.Log(distance);
+        if (distance <= _distance * 12)
+        {
+            animator.SetBool("Isattack", true);
+            bisAttacking = true;
+        }
+        else
+        {
+            animator.SetBool("Isattack", false);
+            bisAttacking = false;
+        }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
