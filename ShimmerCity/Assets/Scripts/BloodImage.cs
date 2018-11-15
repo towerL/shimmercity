c7cee864bb95d3611a6fd57cab074acdb218da53
La745739773 <745739773@qq.com>
2018-11-15 16:03:52 +0800

    blood bar test

---
+++ ShimmerCity/Assets/Scripts/BloodImage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BloodImage : RawImage
+{
+    private Slider _BloodSlider;
+    protected override void OnRectTransformDimensionsChange()
+    {
+        if (_BloodSlider == null)
+            _BloodSlider = transform.parent.parent.GetComponent<Slider>();
+
+        //获取血条的值
+        if (_BloodSlider != null)
+        {
+            //刷新血条的显示
+            float value = _BloodSlider.value;
+            uvRect = new Rect(0, 0, value, 1);
+        }
+    }
+}
