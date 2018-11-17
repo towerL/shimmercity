429de4c56af964cc311a5f5b212c0daeb28ba8f6
towerL <513298163@qq.com>
2018-11-17 21:02:38 +0800

    增加 玩家碰到switch，上面那扇门关闭

---
+++ ShimmerCity/Assets/Scripts/SwitchController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour
+{
+
+    private Animator door_ani;
+
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            var door = GameObject.Find("door1");
+            door.GetComponent<Animator>().SetBool("IsDoorOpen", false);
+        }
+    }
+}
