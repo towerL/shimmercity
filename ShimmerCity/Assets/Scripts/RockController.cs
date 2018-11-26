55e260e0d7a58268ba4bfb067bd156af32740820
towerL <513298163@qq.com>
2018-11-26 22:08:29 +0800

    添加新场景

---
+++ ShimmerCity/Assets/Scripts/RockController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockController : MonoBehaviour {
+    //移动初始位置
+    public float h0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //var v = transform.position;
+        //v.y = Mathf.PingPong(h0, -13);
+        if (transform.position.y < -8.0f)
+        {
+            var v = transform.position;
+            v.y = h0;
+            transform.position = v;
+        }
+	}
+}
---
+++ ShimmerCity/Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            var door = GameObject.Find("door2");
+            //bool isdooropen = door.GetComponent<Animator>().GetBool("IsDoorOpen");
+            //if (isdooropen) {
+            //    SceneManager.LoadScene("Part2_1");
+            //}
+            door.GetComponent<Animator>().SetBool("IsDoorOpen", true);
+            SceneManager.LoadScene("Part2_1");
+        }
+    }
+}
