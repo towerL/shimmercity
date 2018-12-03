6a168467fcaf32f41ce40389a8eabdf5a2db1de1
towerL <513298163@qq.com>
2018-12-03 16:42:05 +0800

    场景2增加三角形石头掉落脚本

---
+++ ShimmerCity/Assets/Scripts/FallenStoneController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallenStoneController : MonoBehaviour {
+    public string stone_name;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.tag == "Stone")
+    //    {
+    //        var falling_stone = GameObject.Find(stone_name);
+    //        falling_stone.GetComponent<SpriteRenderer>().enabled = false;
+    //        gameObject.GetComponent<SpriteRenderer>().enabled = true;
+    //    }
+    //}
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Stone")
+        {
+            var falling_stone = GameObject.Find(stone_name);
+            falling_stone.GetComponent<SpriteRenderer>().enabled = false;
+            gameObject.GetComponent<SpriteRenderer>().enabled = true;
+            falling_stone.GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+}
