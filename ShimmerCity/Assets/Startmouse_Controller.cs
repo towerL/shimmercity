5287ebdc3f31fcd1b727cb67f785e8042ec1ec28
La745739773 <745739773@qq.com>
2018-12-10 21:21:15 +0800

    add startMouse

---
+++ ShimmerCity/Assets/Startmouse_Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Startmouse_Controller : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public float _HP;
+    public Vector2 direction;
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("loadBullet",0,2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void loadBullet()
+    {
+        GameObject mPrefab = (MonoBehaviour.Instantiate(bulletPrefab, Vector3.zero, Quaternion.identity) as GameObject);
+        mPrefab.GetComponent<SpriteRenderer>().sortingOrder = 5;
+        mPrefab.transform.position = transform.position;
+        mPrefab.SendMessage("SetDirection", direction);
+    }
+}
