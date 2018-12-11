140e8a28dffc55f78de1fdd3edb9da4b181b4071
VingtDylan <1305373709@qq.com>
2018-12-11 20:06:31 +0800

    bottle

--- ShimmerCity/Assets/Scripts/Feet_handler3.cs
+++ ShimmerCity/Assets/Scripts/Feet_handler3.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Feet_handler3 : MonoBehaviour {
 	public void OnCollisionEnter2D(Collision2D col){
-		if (col.collider.tag == "Ground" ) {
+		if (col.collider.tag == "Ground" || col.collider.tag == "Brick" || col.collider.tag == "StaticBrick") {
 			SendMessageUpwards ("SetGround", true);
 		} 
 	}
 
 	public void OnCollisionExit2D(Collision2D col){
-		if (col.collider.tag == "Ground" ) {
+		if (col.collider.tag == "Ground" || col.collider.tag == "Brick" || col.collider.tag == "StaticBrick") {
 			SendMessageUpwards("SetGround",false);
 		}
 	}
---
+++ ShimmerCity/Assets/brick_handler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brick_handler : MonoBehaviour {
+
+	private BoxCollider2D brick_collider;
+	private float show_time;
+	private float disappear_time;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
