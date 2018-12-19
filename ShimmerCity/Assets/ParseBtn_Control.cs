96313210c581be3606d6f3b4cbe213536a159607
La745739773 <745739773@qqcom>
2018-12-19 16:47:47 +0800

    增加了暂停和提示的UI

---
+++ ShimmerCity/Assets/ParseBtn_Control.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class ParseBtn_Control : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Image image;
+    public Sprite[] sprites;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        image.sprite = sprites[0];
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        image.sprite = sprites[1];
+    }
+
+
+}
