08888ba8be6c442030438f9a45bf1a01b2f9378f
XZMADAO <826021077@qq.com>
2018-12-21 20:49:52 +0800

    bullet2_lookatplayer

--- ShimmerCity/Assets/Scripts/bullet_2.cs
+++ ShimmerCity/Assets/Scripts/bullet_2.cs
@@ -9,22 +9,27 @@ public class bullet_2 : MonoBehaviour {
     float movevx = 0.5f;
     float movevy = 0.5f;
     float maxspeed = 2.0f;
+    float MAX_ROTATION = 90;
+    private static float MAX_ROTATION_FRAME;
     GameObject player;
     // Use this for initialization
     void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
+        MAX_ROTATION_FRAME =MAX_ROTATION / ((float)(Application.targetFrameRate == -1 ? 60 : Application.targetFrameRate));
     }
 	
 	// Update is called once per frame
 	void Update () {
+        /*
         float x_e = this.transform.position.x;
         float x_p = player.transform.position.x;
         if (x_p > x_e)
             this.transform.localEulerAngles = new Vector3(0, -180, 0);
         else
             this.transform.localEulerAngles = new Vector3(0, 0, 0);
+            */
         bulletmove();
-        //lookattarget();
+        lookattarget();
         //this.transform.LookAt(player.transform);
         Destroy(gameObject, 8);
     }
@@ -41,21 +46,29 @@ public class bullet_2 : MonoBehaviour {
 
     void lookattarget()
     {
-        float zAngles;
-        if (movevy == 0)
-        {
-            zAngles = movevx >= 0 ? -90 : 90;
-        }
-        zAngles = Mathf.Atan(movevx / movevy) * (-180 / Mathf.PI);
-        if (movevy < 0)
-        {
-            zAngles = zAngles - 180;
-        }
-        Vector3 tempAngles = new Vector3(0, 0, zAngles);
-        Quaternion tempQua = this.transform.rotation;
-        tempQua.eulerAngles = tempAngles;
-        this.transform.rotation = tempQua;
+        float dx = player.transform.position.x - this.transform.position.x;
+        float dy = player.transform.position.y - this.transform.position.y;
+        float rotationZ = Mathf.Atan2(dy, dx) * 180 / Mathf.PI;//得到最终的角度并且确保在 [0, 360) 这个区间内
+        rotationZ -= 90;
+        rotationZ = MakeSureRightRotation(rotationZ);//获取增加的角度
+        float originRotationZ = MakeSureRightRotation(this.transform.eulerAngles.z);
+        float addRotationZ = rotationZ - originRotationZ;//超过 180 度需要修改为负方向的角度
+        if (addRotationZ > 180)
+          {
+             addRotationZ -= 360;
+           }//不超过每帧最大可旋转的阀值
+        addRotationZ = Mathf.Max(-MAX_ROTATION_FRAME, Mathf.Min(MAX_ROTATION_FRAME, addRotationZ));
+              //应用旋转
+        this.transform.eulerAngles = new Vector3(0, 0, this.transform.eulerAngles.z + addRotationZ);
     }
+
+    private float MakeSureRightRotation(float rotation)
+    {
+        rotation += 360;
+        rotation %= 360;
+        return rotation;
+      }
+
     void bulletmove()
     {
         float vx = player.transform.position.x - this.transform.position.x;
