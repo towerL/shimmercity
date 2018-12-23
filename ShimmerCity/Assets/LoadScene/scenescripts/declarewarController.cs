0333d103c30d6a07f2210c4d38e9ccb2fb67fd2c
towerL <513298163@qq.com>
2018-12-23 13:49:00 +0800

    场景UI

---
+++ ShimmerCity/Assets/LoadScene/scenescripts/declarewarController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+public class declarewarController : MonoBehaviour {
+    bool flag = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+    //fade脚本
+    void setPara(GameObject Object, float speed, float time, bool f)
+    {
+        Object.GetComponent<fadein_out>().FadeSpeed = speed;
+        Object.GetComponent<fadein_out>().WaitTime = time;
+        Object.GetComponent<fadein_out>().flag = f;
+    }
+
+    public void AddEventTrigger(Transform insObject, EventTriggerType eventType, UnityEngine.Events.UnityAction<BaseEventData> myFunction)//泛型委托
+    {
+        EventTrigger trigger = insObject.GetComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = eventType;
+        entry.callback.AddListener(myFunction);
+        trigger.triggers.Add(entry);
+    }
+
+    void myfun(BaseEventData eventData)
+    {
+        var OK = GameObject.Find("Y-3_03icon");
+        OK.GetComponent<onclickdestroy>().MouseClick();
+    }
+
+    void myfun2(BaseEventData eventData)
+    {
+        var OK = GameObject.Find("Y-3_03icon");
+        OK.GetComponent<onclickdestroy>().MouseEnter();
+    }
+    void myfun3(BaseEventData eventData)
+    {
+        var OK = GameObject.Find("Y-3_03icon");
+        OK.GetComponent<onclickdestroy>().MouseExit();
+    }
+	// Update is called once per frame
+	void Update () {
+        var boss = GameObject.Find("boss");
+        flag = boss.GetComponent<Animator>().GetBool("HasShown");
+        if (flag) {
+            var bg4 = GameObject.Find("Y-3_01bg");
+            var box = GameObject.Find("Y-3_02box");
+            var icon = GameObject.Find("Y-3_03icon");
+
+            bg4.AddComponent<fadein_out>();
+            setPara(bg4, 2.0f, 0.0f, true);
+
+            box.AddComponent<fadein_out>();
+            setPara(box, 2.0f, 0.3f, true);
+
+            icon.AddComponent<fadein_out>();
+            setPara(icon, 2.0f, 0.8f, true);
+
+            var btn = GameObject.Find("Button_pk");
+            EventTrigger trigger = btn.GetComponent<EventTrigger>();
+            AddEventTrigger(btn.transform, EventTriggerType.PointerClick, myfun);
+            AddEventTrigger(btn.transform, EventTriggerType.PointerEnter, myfun2);
+            AddEventTrigger(btn.transform, EventTriggerType.PointerExit, myfun3);
+        }
+	}
+}
